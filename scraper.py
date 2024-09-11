fcfa4f01a78016a99adc73dea168d360be89d18f
Bolaji Joseph Ogaji <bolajijoseph07@gmail.com>
2024-09-11 22:28:03 +0100

    Add delay to conform with site scraping rules

--- scraper.py
+++ scraper.py
@@ -3,6 +3,7 @@ import requests
 import pandas
 from io import StringIO
 from pprint import pprint
+import time
 
 # def get_team_data(league_uri, league_id, season, file, number_of_teams=10):
 #     base_url = "https://fbref.com"
@@ -42,6 +43,7 @@ from pprint import pprint
 # get_team_data("Premier-League-Stats", "9", "2023-2024", "teams.csv")
 
 response = requests.get("https://fbref.com/en/squads/b8fd03ef/Manchester-City-Stats")
+time.sleep(4)
 new_soup = BS(response.content, 'html.parser')
 
 big_logo = new_soup.find('img', class_='teamlogo').get('src')
@@ -49,6 +51,7 @@ big_logo = new_soup.find('img', class_='teamlogo').get('src')
 squad_data = new_soup.find('table', id='stats_standard_9').find('tbody').find_all('tr')
 
 squad = []
+indicator = 0
 for player in squad_data:
     name = player.find('th').get_text()
     player_page = player.find('th').find('a').get('href')
@@ -57,6 +60,7 @@ for player in squad_data:
     age = player.find('td', {'data-stat': 'age'}).get_text()
 
     temp_response = requests.get(f'https://fbref.com{player_page}')
+    time.sleep(4)
     temp_soup = BS(temp_response.content, 'html.parser')
     picture = temp_soup.find('div', class_='media-item').find('img').get('src')
 
@@ -67,6 +71,8 @@ for player in squad_data:
         'country': country,
         'age': age.split('-')[0]
     }
+    indicator += 1
     squad.append(player_data)
+    print(indicator)
 
 print(squad)
