88e055b770dfb4f2d7f28ea27e29bc7505dcf357
Bolaji Joseph Ogaji <bolajijoseph07@gmail.com>
2024-09-12 22:26:07 +0100

    expanded project to scrape individual players' data

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
-About the project 
-====================
+# About the project
 
-This is a webscraping project built in Python.
------------------------------------------------
+## This is a webscraping project built in Python.
 
 - The project is built to scrape the [fbref website](https://fbref.com/en/) to get basic team and player data
 - The project makes use of the BeautifulSoup, pandas, and requests library in order to successfully scrape the website to provide data that powers applications that require it
 
-Get started
-----------------
+## Get started
 
 ## Table of Contents
 
@@ -19,41 +16,52 @@ Get started
 ## Installation
 
 1. Clone the repository:
-    ```bash
-    git clone https://github.com/Ace7codes/football-data-webscraping.git
-    cd football-data-webscraping
-    ```
+
+   ```bash
+   git clone https://github.com/Ace7codes/football-data-webscraping.git
+   cd football-data-webscraping
+   ```
 
 2. Create and activate a virtual environment (optional but recommended):
-    ```
-    FOR MAC
-    python3 -m venv venv
-    source venv/bin/activate 
-    ```
 
-    ```
-    FOR WINDOWS
-    python -m venv venv
-    source venv/Scripts/activate
-    ```
+   ```
+   FOR MAC
+   python3 -m venv venv
+   source venv/bin/activate
+   ```
+
+   ```
+   FOR WINDOWS
+   python -m venv venv
+   source venv/Scripts/activate
+   ```
 
 3. Install the required packages:
-    ```bash
-    pip install -r requirements.txt
-    ```
+   ```bash
+   pip install -r requirements.txt
+   ```
 
 ## Usage
 
-1. Run the scraper script:
-    ```bash
-    python scraper.py
-    ```
+1. Open the python script
 
-2. The scraped data will be saved in a CSV file in the `data` directory.
+   ```scraper.py
+
+   ```
+
+2. Insert the required arguements to the function call at the end of the script
+
+3. Run the scraper script:
+
+   ```bash
+   python scraper.py
+   ```
+
+4. The scraped data will be saved in a CSV file in the `data` directory.
 
 ## Project Structure
+
 - `data/`: Directory where the scraped data is stored.
 - `scraper.py`: Main script for scraping data.
 - `requirements.txt`: List of dependencies required for the project.
 - `README.md`: Project documentation.
-
--- scraper.py
+++ scraper.py
@@ -2,77 +2,78 @@ from bs4 import BeautifulSoup as BS
 import requests
 import pandas
 from io import StringIO
-from pprint import pprint
 import time
 
-# def get_team_data(league_uri, league_id, season, file, number_of_teams=10):
-#     base_url = "https://fbref.com"
-#     data = requests.get(f"{base_url}/en/comps/{str(league_id)}/{str(season)}/{str(season)}-{str(league_uri)}")
-
-#     soup = BS(data.content, 'html.parser')
-#     table = soup.find("table", id=f'results{str(season)}{league_id}1_overall')
-
-#     left_side = table.find_all('td', class_='left')
-
-#     links = []
-#     for td in left_side:
-#         links.extend(td.find_all(['img', 'a']))
-
-
-#     images = list(filter(None, [l.get("src") for l in links]))
-#     club_names = list(filter(lambda x: x != '', [l.get_text() for l in links]))
-
-#     uris = list(filter(None, [l.get('href') for l in links]))
-#     squad_uris = [uri.replace(f'{str(season)}/', '') for uri in uris]
-
-#     squad_urls = [base_url+uri for uri in squad_uris]
-
-#     # Using range 10 because I only want the top 10
-#     data_dict = [
-#         {"team_name": club_names[i], "logo": images[i], "squad_link": squad_urls[i]}
-#         for i in range(number_of_teams)
-#     ]
-
-#     team_data = pandas.DataFrame(data_dict)
-
-#     team_data.to_csv(f"data/{str(file)}", index=False)
-
-#     return
-
-
-# get_team_data("Premier-League-Stats", "9", "2023-2024", "teams.csv")
-
-response = requests.get("https://fbref.com/en/squads/b8fd03ef/Manchester-City-Stats")
-time.sleep(4)
-new_soup = BS(response.content, 'html.parser')
-
-big_logo = new_soup.find('img', class_='teamlogo').get('src')
-
-squad_data = new_soup.find('table', id='stats_standard_9').find('tbody').find_all('tr')
-
-squad = []
-indicator = 0
-for player in squad_data:
-    name = player.find('th').get_text()
-    player_page = player.find('th').find('a').get('href')
-    position = player.find('td', {'data-stat': 'position'}).get_text()
-    country = player.find('td', {'data-stat': 'nationality'}).get_text()
-    age = player.find('td', {'data-stat': 'age'}).get_text()
-
-    temp_response = requests.get(f'https://fbref.com{player_page}')
+def get_team_data(league_uri, league_id, season, file, number_of_teams=10):
+    base_url = "https://fbref.com"
+    data = requests.get(f"{base_url}/en/comps/{str(league_id)}/{str(season)}/{str(season)}-{str(league_uri)}")
     time.sleep(4)
-    temp_soup = BS(temp_response.content, 'html.parser')
-    picture = temp_soup.find('div', class_='media-item').find('img').get('src')
+    soup = BS(data.content, 'html.parser')
+    table = soup.find("table", id=f'results{str(season)}{league_id}1_overall')
 
-    player_data = {
-        'name': name,
-        'position': position.split(',')[0],
-        'picture': picture,
-        'country': country,
-        'age': age.split('-')[0]
-    }
-    indicator += 1
-    squad.append(player_data)
-    print(indicator)
+    left_side = table.find_all('td', class_='left')
 
-print(squad)
+    links = []
+    for td in left_side:
+        links.extend(td.find_all('a'))
+
+    club_names = list([l.get_text() for l in links])
+
+    uris = list([l.get('href') for l in links])
+    squad_uris = [uri.replace(f'{str(season)}/', '') for uri in uris]
+
+    squad_urls = [base_url+uri for uri in squad_uris]
+
+
+    data_dict = [
+        {"team_name": club_names[i], "squad_link": squad_urls[i]}
+        for i in range(number_of_teams)
+    ]
+    squad = []
+
+    for team in data_dict:
+        response = requests.get(team['squad_link'])
+        time.sleep(4)
+        new_soup = BS(response.content, 'html.parser')
+
+        big_logo = new_soup.find('img', class_='teamlogo').get('src')
+
+        squad_data = new_soup.find('table', id='stats_standard_9').find('tbody').find_all('tr')
+
+
+        for player in squad_data:
+            name = player.find('th').get_text()
+            player_page = player.find('th').find('a').get('href')
+            position = player.find('td', {'data-stat': 'position'}).get_text()
+            country = player.find('td', {'data-stat': 'nationality'}).get_text()
+            age = player.find('td', {'data-stat': 'age'}).get_text()
+
+            temp_response = requests.get(f'{base_url}{player_page}')
+            time.sleep(4)
+            temp_soup = BS(temp_response.content, 'html.parser')
+            media_item = temp_soup.find('div', class_='media-item')
+            if media_item:
+                picture = media_item.find('img').get('src')
+            else:
+                picture = 'https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg?t=st=1726169296~exp=1726172896~hmac=c369fdbb9efe89fcdb13b0bd15bc6b62b3db3fad7e4b8a52064909e6b818e930&w=826'
+
+            
+            player_data = {
+                    'team_name' : team['team_name'],
+                    'team_logo' : big_logo,
+                    'name': name,
+                    'position': position.split(',')[0],
+                    'picture': picture,
+                    'country': country,
+                    'age': age.split('-')[0]
+                }
+            squad.append(player_data)
+
+    team_data = pandas.DataFrame(squad)
+    team_data.to_csv(f"data/{str(file)}", index=False)
+
+    print('Script executed successfully!\nView .csv file to see the data')
+    return
+
+# Pass arguements to the function below
+get_team_data("Premier-League-Stats", "9", "2023-2024", "premier_league_test.csv", 1)
